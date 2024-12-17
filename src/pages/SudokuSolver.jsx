6f177a2a24002e2b709590ae236891e7e0fea740
Peter Jiang <pzjiang01@hotmail.com>
2024-12-17 16:15:22 -0500

    complete logic

--- src/pages/SudokuSolver.jsx
+++ src/pages/SudokuSolver.jsx
@@ -12,6 +12,11 @@ const SudokuSolver = () => {
     const codeNine = 57;
     const codeOne = 49;
     const inputRef = useRef(new Array(81));
+    const maskX = new Array(10);
+    const maskY = new Array(10);
+    const maskSquare = new Array(10);
+    const maskVals = [1,2,4,8,16,32,64,128,256];
+    const maskTotal = 511;
     
 
     useEffect(() => {
@@ -29,11 +34,59 @@ const SudokuSolver = () => {
             }
             setSolvedGrid(newSolvedGrid);
             setSudokuGrid(newGrid);
+            for (let i = 0; i < 9; i++) {
+                maskX[i] = 0;
+                maskY[i] = 0;
+                maskSquare[i] = 0;
+            }
         }
   
         setLoadState(1);
-        enableGrid();
+        enableGrid(clearGrid);
         setWarningMessage('');
+    }
+
+    const calcSquareMask = (x, y) => {
+        let cornerX = x - (x % 3);
+        let cornerY = y - (y % 3);
+        return cornerX + Math.floor(cornerY / 3);
+    }
+
+    // false means already taken
+    const fillMask = (x, y, val) => {
+        const maskVal = maskVals[val -1];
+        const squareMask = calcSquareMask(x,y);
+        if (maskX[x] & maskVal || maskY[y] & maskVal || maskSquare[squareMask] & maskVal) {
+            return false;
+        }
+        maskX[x] = maskX[x] | maskVal;
+        maskY[y] = maskY[y] | maskVal;
+        maskSquare[squareMask] = maskSquare[squareMask] | maskVal;
+        return true;
+    }
+
+    const removeMask = (x, y, val) => {
+        const maskVal = maskVals[val - 1];
+        const squareMask = calcSquareMask(x,y);
+        maskX[x] = maskX[x] - maskVal;
+        maskY[y] = maskY[y] - maskVal;
+        maskSquare[squareMask] = maskSquare[squareMask] - maskVal;
+    }
+
+    const countPossibleEntries = (ind) => {
+        const x = Math.floor(ind / 9);
+        const y = ind % 9;
+        let totalPosMask = 0;
+        totalPosMask = totalPosMask | maskX[x];
+        totalPosMask = totalPosMask | maskY[y];
+        totalPosMask = totalPosMask | maskSquare[calcSquareMask(x,y)];
+        let counter = 9;
+        for (let i = 0; i < 9; i++) {
+            if (maskVals[i] & totalPosMask) {
+                counter --;
+            }
+        }
+        return counter;
     }
 
     const inputChanged  = (event, x, y) => {
@@ -53,56 +106,58 @@ const SudokuSolver = () => {
     const preProcess = () => {
         //check that there are at least 17 clues
         let counter = 0;
-        const filledSquares = [];
+
+        let x;
+        let y;
         for (let i = 0; i < sudokuGrid.length; i++) {
             if (sudokuGrid[i] !== '') {
                 counter ++;
+                x = Math.floor(i / 9);
+                y = i % 9;
                 solvedGrid[i] = sudokuGrid[i];
-                filledSquares.push(i);
+                if (!fillMask(x, y, solvedGrid[i])) {
+                    setWarningMessage('The clues do not make up a valid sudoku grid');
+                    return false;
+                }
+
             }
         }
-        /*
+        
         if (counter < 17) {
             setWarningMessage("Must have at least 17 clues for a unique solution");
             return false;
-        }*/
-        for (let i = 0; i < counter; i++) {
-            if (!verifyPossible(filledSquares[i])) {
-                setWarningMessage('The clues do not make up a valid sudoku grid');
-                return false;
-            }
         }
+        
        
        return true;
     }
-
+    /*
     const verifyPossible = (ind) => {
         
-        if (sudokuGrid[ind] === '') {
+        if (solvedGrid[ind] === '') {
             return true;
         }
         const x = Math.floor(ind / 9);
         const y = ind % 9;
-        if (!verifySquare(x, y)) {
-            return false;
-        }
-        if (!verifyRow(x,y)) {
+        let val = solvedGrid[ind];
+        let maskVal = maskVals[val - 1];
+        console.log(maskX[x], maskY[y], maskSquare[calcSquareMask(x,y)], maskVal, x, y);
+        if (maskX[x] & maskVal  || maskY[y] & maskVal || maskSquare[calcSquareMask(x, y)] & maskVal) {
             return false;
         }
 
         return true;
     }
+    
     const verifySquare = (x, y) => {
         const cornerX = x - (x % 3);
         const cornerY = y - (y % 3);
-        
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
                 if ((cornerX + i) === x && (cornerY + j) === y) {
                     continue;
                 }
                 if (solvedGrid[realInd(cornerX + i, cornerY + j)] === solvedGrid[realInd(x, y)]) {
-
                     console.log("failed square");
                     return false;
                 }
@@ -111,69 +166,85 @@ const SudokuSolver = () => {
         return true;
     }
     const verifyRow = (x, y) => {
-        for (let i = 1; i < 10; i++) {
+        for (let i = 0; i < 9; i++) {
             //check row and column
             
             if (i !==x && solvedGrid[realInd(i,y)] === solvedGrid[realInd(x,y)]) {
-                console.log("failed x");
                 return false;
             }
             if (i !== y && solvedGrid[realInd(x, i)] === solvedGrid[realInd(x,y)]) {
-                console.log("failed y");
                 return false;
             }
         }
         return true;
-    }
-    const recursiveSudoku = async (ind) => {
-        if (ind === 82) {
+    }*/
+   
+    
+    const solveSudoku = () => {
+        let minInd = -1;
+        let minCounter = 10;
+        let counter;
+        for (let i = 0; i < 81; i++) {
+            if (solvedGrid[i] !== '') {
+                continue;
+            }
+            counter = countPossibleEntries(i);
+            if (counter === 0) {
+                console.log("counter hit 0 for some reason");
+                return false;
+            }
+            if (counter < minCounter) {
+                minCounter = counter;
+                minInd = i;
+            }
+        }
+        if (minInd === -1) {
+            //all squares filled;
             return true;
         }
-        if (sudokuGrid[ind] !== '') {
-            // if this is preset clue
-            if (recursiveSudoku(ind + 1)) {
-                return true;
-            }
-            return false;
-        }
-        else {
-            for (let i = 1; i < 10; i++) {
-                solvedGrid[ind] = i;
-                if (!verifyPossible(ind)) {
-                    continue;
-                }
-                if (recursiveSudoku(ind + 1)) {
+        const x = Math.floor(minInd / 9);
+        const y = minInd % 9;
+
+        for (let i = 1; i < 10; i++) {
+            if (fillMask(x,y,i)) {
+                solvedGrid[minInd] = i;
+                if (solveSudoku()) {
                     return true;
                 }
+                removeMask(x,y,i);
             }
         }
+        solvedGrid[minInd] = '';
+
         return false;
     }
-    const backtrackSudoku = async () => {
-
-        const solFound = await recursiveSudoku(0);
-        if (!solFound) {
-            setWarningMessage("Was not able to find a valid solution to this puzzle");
-            setLoadState(1);
-        }
-        setLoadState(3);
-    }
-    const solveSudoku = async () => {
+    const solvePuzzle = () => {
+        //initial start to solving the puzzle
         if (!preProcess()) {
             setLoadState(1);
             return;
         }
-        
-        console.log("passed preprocess");
-        backtrackSudoku();
-
-    }
-    const solvePuzzle = () => {
-        //initial start to solving the puzzle
-        
+        //console.log(solvedGrid);
         if (loadState === 1) {
             setLoadState(2);
-            solveSudoku();
+            setTimeout(()=>{/*
+                solveSudoku(0).then((val) => {
+                    if (!val) {
+                        setWarningMessage("No valid solution was found");
+                        setLoadState(1);
+                    }else {
+                        setLoadState(3);
+                    }
+                });*/
+                const solFound = solveSudoku();
+                if (solFound) {
+                    setLoadState(3);
+                }
+                else {
+                    setWarningMessage("No valid solution was found");
+                    setLoadState(1);
+                }
+            }, 0);
         } 
         disableGrid();
     }
@@ -184,12 +255,14 @@ const SudokuSolver = () => {
             element.disabled = true;
         });
     }
-    const enableGrid = () => {
+    const enableGrid = (clearGrid) => {
         const inputElements = document.getElementsByName('sudokuEntry');
 
         inputElements.forEach((element) => {
             element.disabled = false;
-            element.value = '';
+            if(clearGrid ) {
+                element.value = '';
+            }
         });
     }
     const onKeyDownEntry = (key,x,y) => {
@@ -265,7 +338,8 @@ const SudokuSolver = () => {
                                 return ( <div className="_sudokuDisplayRow">
                                 {arrNine.map((value2) => {
                                     return (
-                                        <div className={` _displayBorder _sudokuEntryField ${value%3 === 0 ? '_pdBottom' : ""} ${value2%3 === 0 ? '_pdRight' : ""}` }>{solvedGrid[realInd(value - 1, value2 - 1)]}</div>
+                                        <div className={` _displayBorder _sudokuEntryField ${value%3 === 0 ? '_pdBottom' : ""} ${value2%3 === 0 ? '_pdRight' : ""}` }>
+                                            {solvedGrid[realInd(value - 1, value2 - 1)]}</div>
                                     )
                                 })}
                             </div>)
@@ -280,6 +354,13 @@ const SudokuSolver = () => {
         </>
     );
 }
-
+//TODO:
+/**
+ * 1. Modify validation to use bitmask
+ * 2. implement function to find square bitmask ind
+ * 3. update preprocess to fill out bitmask
+ * 4. when using backtracking, find the ind with the least possible options, and start there
+ * 
+ */
 
 export default SudokuSolver;
